490970371279678c147054a1dcbd034e33f9d420
Sivakumar Vunnam <sivakumar.vunnam@epsilon.com>
2021-06-25 14:44:18 -0500

    custom entrypoint to always use hosts user UID and GID

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+# set uid and gid with same permissions as mount point
+USER_ID=${TERRAFORM_UID:-$(stat -c %u "$(pwd)")}
+GROUP_ID=${TERRAFORM_GID:-$(stat -c %g "$(pwd)")}
+USER_NAME=${TERRAFORM_USER:-terraform}
+GROUP_NAME=${TERRAFORM_GROUP:-terraform}
+
+# change to new home directory
+export HOME=/home/${USER_NAME}
+
+# only create new user if we haven't docker remapped
+# https://docs.docker.com/engine/security/userns-remap/
+if [ "${USER_ID}" == "0" ]; then
+  terraform "$@"
+else
+  # add our custom group and user
+  addgroup -S -g "${GROUP_ID}" "${GROUP_NAME}"
+  adduser -S -s /bin/bash -u "${USER_ID}" -G "${GROUP_NAME}" "${USER_NAME}"
+
+  # run commands with new UID and GID
+  exec gosu "${USER_NAME}" terraform "$@"
+fi
